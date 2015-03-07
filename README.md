175107bb38305b0656639bbc1b50264311c48b85
Dave Andreoli <dave@gurumeditation.it>
2015-03-07 18:52:31 +0100

    Welcome prompter !

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# prompter
-A bash prompt framework
+# A bash prompt framework
+
+This is a simple infrastructure to customize your bash prompt, it comes with
+some predefined prompts that you can use or customize at your wish
+
+## Install
+
+There is not real installation requirements, just clone the repo in your home
+folder (or make a link to it) and add this line to your bashrc:
+
+    source ~/prompter/prompter.bash
+
+## Usage
+
+Select your prompt using the *prompter_select* command
+
+Or create your own just by creating a new file in prompter/configs/prompts
---
+++ prompter.bash
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+PROMPTER_PLUGINS_DIR="${HOME}/prompter/plugins"
+PROMPTER_CONFIGS_DIR="${HOME}/prompter/configs"
+
+declare -A PROMPTER_COLS
+PROMPTER_COLS[OFF]="\e[0m"
+PROMPTER_COLS[LIGHT]="\e[1m"
+PROMPTER_COLS[UNDERLINE]="\e[4m"
+# foregrounds
+PROMPTER_COLS[GRAY]="\e[30m"
+PROMPTER_COLS[RED]="\e[31m"
+PROMPTER_COLS[GREEN]="\e[32m"
+PROMPTER_COLS[YELLOW]="\e[33m"
+PROMPTER_COLS[BLUE]="\e[34m"
+PROMPTER_COLS[MAGENTA]="\e[35m"
+PROMPTER_COLS[CYAN]="\e[36m"
+PROMPTER_COLS[WHITE]="\e[37m"
+# backgrounds
+PROMPTER_COLS[GRAYBG]="\e[40m"
+PROMPTER_COLS[REDBG]="\e[41m"
+PROMPTER_COLS[GREENBG]="\e[42m"
+PROMPTER_COLS[YELLOWBG]="\e[43m"
+PROMPTER_COLS[BLUEBG]="\e[44m"
+PROMPTER_COLS[MAGENTABG]="\e[45m"
+PROMPTER_COLS[CYANBG]="\e[46m"
+PROMPTER_COLS[WHITEBG]="\e[47m"
+
+declare -A PROMPTER_PLUGINS
+
+function _prompter_read_plugins
+{
+    local filename
+    for filename in $PROMPTER_PLUGINS_DIR/*; do
+        source $filename
+        PROMPTER_PLUGINS[$NAME]=$OUT
+        unset NAME DESC OUT
+    done
+}
+
+function prompter_apply
+{
+    local name=`cat $PROMPTER_CONFIGS_DIR/current`
+    source "$PROMPTER_CONFIGS_DIR/prompts/$name"
+    local prompt=$PROMPT
+    unset NAME DESC PROMPT
+    for name in "${!PROMPTER_PLUGINS[@]}"; do
+        prompt=${prompt//#${name}#/${PROMPTER_PLUGINS[$name]}}
+    done
+    for name in "${!PROMPTER_COLS[@]}"; do
+        prompt=${prompt//_${name}_/${PROMPTER_COLS[$name]}}
+    done
+    export PS1="${prompt}${PROMPTER_COLS[OFF]}"
+}
+
+function prompter_select
+{
+    echo "Available prompts:"
+    local prompts=()
+    local i=1
+    local filename
+    local current=`cat $PROMPTER_CONFIGS_DIR/current`
+    local cur
+    for filename in $PROMPTER_CONFIGS_DIR/prompts/*; do
+        source $filename
+        prompts[$i]=$NAME
+        [ ${prompts[$i]} == $current ] && cur="*" || cur=" "
+        echo "$i) ${cur}${prompts[$i]} ($DESC)"
+        let i++
+        unset NAME DESC PROMPT
+    done
+    # echo "[Create Delete Modify]"
+    read -p "Make your choice: "
+    case $REPLY in
+        # c|C) echo "CREATE";;
+        # d|D) echo "DELETE";;
+        # m|M) echo "MODIFY";;
+        *)
+            echo ${prompts[$REPLY]} > $PROMPTER_CONFIGS_DIR/current
+            prompter_apply
+        ;;
+    esac
+}
+
+# main
+_prompter_read_plugins
+prompter_apply
